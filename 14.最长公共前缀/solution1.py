872f4db2fbc282fee8345936396cfeac4377ebbd
Idealisten <prometheus.mr.cy@gmail.com>
2021-12-30 00:06:45 +0800

    14

--- 14.最长公共前缀/solution1.py
+++ 14.最长公共前缀/solution1.py
@@ -2,16 +2,22 @@ class Solution:
     def longestCommonPrefix(self, strs) -> str:
         common = ""
         min_length = len(strs[0])
-        for m in strs:
-            if len(m) < min_length:
-                min_length = len(m)
-        for i in range(0, min_length):
-            for j in range(0, len(strs) - 1):
-                if strs[j][i] != strs[j + 1][i]:
-                    return common
-                else:
+        if len(strs) == 1:
+            return strs[0]
+        else:
+            for m in strs:
+                if len(m) < min_length:
+                    min_length = len(m)
+            for i in range(0, min_length):
+                flag = 1
+                for j in range(1, len(strs)):
+                    if strs[j][i] != strs[0][i]:
+                        flag = 0
+                        return common
+                if flag == 1:
                     common = common + strs[j][i]
+            return common
 
 
 s = Solution()
-print(s.longestCommonPrefix(["flower", "flow", "flight"]))
+print(s.longestCommonPrefix(["flower", "flow", "flight", "f"]))
