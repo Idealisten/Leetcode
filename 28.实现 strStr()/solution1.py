a2ff73cbb352aa70294b55ec2725dffd8701a7b1
Idealisten <prometheus.mr.cy@gmail.com>
2022-01-06 18:40:22 +0800

    28-暴力搜索

--- 28.实现 strStr()/solution1.py
+++ 28.实现 strStr()/solution1.py
@@ -1,16 +1,17 @@
+# 暴力搜索
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         if len(needle) == 0:
             return 0
-        for i in range(len(haystack)-len(needle)+1):
+        for i in range(len(haystack) - len(needle) + 1):
             r = True
             for j in range(len(needle)):
-                if needle[j] != haystack[i+j]:
+                if needle[j] != haystack[i + j]:
                     r = False
                     break
             if r is True:
                 return i
-        return -1
+        return -1  # 如果执行完for循环还没找到匹配的结果或确定不匹配，例如needle长度>haystack长度，则返回-1
 
 
 s = Solution()
