d4a1bc2244c778141ac22ef51bce51367acc455a
Idealisten <prometheus.mr.cy@gmail.com>
2022-03-03 00:26:03 +0800

    100.相同的树——广度优先搜索，队列

---
+++ 100.相同的树/solution2.py
@@ -0,0 +1,51 @@
+#   广度优先搜索，队列
+"""
+也可以通过广度优先搜索判断两个二叉树是否相同。同样首先判断两个二叉树是否为空，如果两个二叉树都不为空，则从两个二叉树的根节点开始广度优先搜索。
+使用两个队列分别存储两个二叉树的节点。初始时将两个二叉树的根节点分别加入两个队列。每次从两个队列各取出一个节点，进行如下比较操作。
+比较两个节点的值，如果两个节点的值不相同则两个二叉树一定不同；
+如果两个节点的值相同，则判断两个节点的子节点是否为空，如果只有一个节点的左子节点为空，或者只有一个节点的右子节点为空，则两个二叉树的结构不同，因此两个二叉树一定不同；
+如果两个节点的子节点的结构相同，则将两个节点的非空子节点分别加入两个队列，子节点加入队列时需要注意顺序，如果左右子节点都不为空，则先加入左子节点，后加入右子节点。
+如果搜索结束时两个队列同时为空，则两个二叉树相同。如果只有一个队列为空，则两个二叉树的结构不同，因此两个二叉树不同。
+链接：https://leetcode-cn.com/problems/same-tree/solution/xiang-tong-de-shu-by-leetcode-solution/
+"""
+# Definition for a binary tree node.
+import collections
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+
+        queue1 = collections.deque([p])
+        queue2 = collections.deque([q])
+
+        while queue1 and queue2:
+            node1 = queue1.popleft()
+            node2 = queue2.popleft()
+            if node1.val != node2.val:
+                return False
+            left1, right1 = node1.left, node1.right
+            left2, right2 = node2.left, node2.right
+            if (not left1) ^ (not left2):  # 亦或运算，相同为1，不相同为0
+                return False
+            if (not right1) ^ (not right2):
+                return False
+            if left1:
+                queue1.append(left1)
+            if right1:
+                queue1.append(right1)
+            if left2:
+                queue2.append(left2)
+            if right2:
+                queue2.append(right2)
+        return not queue1 and queue2  # queue1和queue2都为空时返回真，否则返回假
