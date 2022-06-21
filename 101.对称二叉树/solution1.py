e74e45dccdf1644bf61503fe47c9b300bc5a303f
Idealisten <prometheus.mr.cy@gmail.com>
2022-06-21 23:58:31 +0800

    101 104 108

---
+++ 101.对称二叉树/solution1.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def isSymmetric(self, root: TreeNode):
+        return self.isMirror(root.left, root.right)
+
+    def isMirror(self, left, right):
+        if left is None and right is None:
+            return True
+        elif left is None or right is None:
+            return False
+        else:
+            return (left.val == right.val) and self.isMirror(left.left, right.right) and self.isMirror(left.right, right.left)
+            
+
