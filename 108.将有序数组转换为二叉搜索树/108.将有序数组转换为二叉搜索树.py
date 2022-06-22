d50537ad603b751e9d0846937df0a33b4cd91627
Idealisten <prometheus.mr.cy@gmail.com>
2022-06-22 23:19:17 +0800

    递归

--- 108.将有序数组转换为二叉搜索树/108.将有序数组转换为二叉搜索树.py
+++ 108.将有序数组转换为二叉搜索树/108.将有序数组转换为二叉搜索树.py
@@ -10,7 +10,7 @@ class Solution:
         def helper(left, right):
             if left > right:
                 return None
-            mid = (left+right)/2
+            mid = (left+right)//2
             root = TreeNode(nums[mid])
             root.left = helper(left, mid-1)
             root.right = helper(mid+1, right)
