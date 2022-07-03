3f9bc561388eb4cfd618ed1558a4042da4d58b35
Idealisten <prometheus.mr.cy@gmail.com>
2022-07-04 00:17:56 +0800

    111最小深度深度优先

---
+++ 111.二叉树的最小深度/111.二叉树最小深度——DFS.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minDepth(self, root) -> int:
+        if not root:
+            return 0
+        if not root.left and not root.right:
+            return 1
+        left = self.minDepth(root.left)
+        right = self.minDepth(root.right)
+        if not root.left or not root.right:
+            return left+right+1
+        return min(left, right)+1
