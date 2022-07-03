64b9c00f22dbcdbf3ef3f4b1cdd26a7d0e38ee40
Idealisten <prometheus.mr.cy@gmail.com>
2022-07-04 00:22:06 +0800

    111最小深度深度优先，递归结束条件

--- 111.二叉树的最小深度/111.二叉树最小深度——DFS.py
+++ 111.二叉树的最小深度/111.二叉树最小深度——DFS.py
@@ -1,3 +1,9 @@
+# 递归结束条件
+# 叶子节点的定义是左孩子和右孩子都为 null 时叫做叶子节点
+# 当 root 节点左右孩子都为空时，返回 1
+# 当 root 节点左右孩子有一个为空时，返回不为空的孩子节点的深度
+# 当 root 节点左右孩子都不为空时，返回左右孩子较小深度的节点值
+
 class Solution:
     def minDepth(self, root) -> int:
         if not root:
