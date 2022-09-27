20663392ae7084dd0104cd74ea3b85ba56f77f35
CAOYUE <prometheus.mr.cy@gmail.com>
2022-09-28 00:09:09 +0800

    完成了将中缀表达式转化为后缀表达式

---
+++ 面试题16.26.计算器/后缀表达式.py
@@ -0,0 +1,76 @@
+"""
+    对于普通中缀表达式的计算，我们可以将其转化为后缀表达式再进行计算。转换方法也十分简单。只要建立一个用于存放运算符的栈，扫描该中缀表达式：
+    如果遇到数字，直接将该数字输出到后缀表达式（以下部分用「输出」表示输出到后缀表达式）；
+    如果遇到左括号，入栈；
+    如果遇到右括号，不断输出栈顶元素，直至遇到左括号（左括号出栈，但不输出）；
+    如果遇到其他运算符，不断去除所有运算优先级大于等于当前运算符的运算符，输出。最后，新的符号入栈；
+    把栈中剩下的符号依次输出，表达式转换结束
+"""
+"""
+    后缀表达式的计算：
+    后缀表达式的计算就是从左到右扫描表达式，遇到数字就将其压入栈，遇到操作符表示可以计算，这时取出栈顶的两个元素进行操作，然后再次将结果压入栈，
+    最后栈里会留下一个元素，该元素就是运行结果
+"""
+
+
+class Solution:
+
+    suffix_exp = []
+    operator_stack = []
+
+    def calculate(self, medial_exp):
+        medial_exp = medial_exp.replace(" ", "")
+        lens = len(medial_exp)
+        i = 0
+        while i < lens:
+            if medial_exp[i].isdigit() is True:     # 是数字
+                j = 1
+                x = i
+                num = medial_exp[i]
+                while j < lens - x:
+                    if medial_exp[x+j].isdigit() is True:
+                        num = num + medial_exp[x+j]
+                        i += 1
+                        j += 1
+                    else:
+                        break
+                self.suffix_exp.append(num)
+                i += 1
+            else:   # 是运算符或括号
+                if medial_exp[i] == "(":
+                    self.operator_stack.append(medial_exp[i])
+                elif medial_exp[i] == ")":
+                    while self.operator_stack[len(self.operator_stack)-1] != "(":
+                        self.suffix_exp.append(self.operator_stack.pop())
+                    self.operator_stack.pop()
+                else:   # 中缀表达式此时的运算符是+-*/
+                    if len(self.operator_stack) == 0:   # 符号栈为空
+                        self.operator_stack.append(medial_exp[i])
+                    else:   # 符号栈非空
+
+                        if medial_exp[i] == "*" or medial_exp == "/":   # 中缀表达式中此时运算符是*或/
+                            while self.operator_stack:
+                                cur_operator = self.operator_stack.pop()
+                                self.operator_stack.append(cur_operator)
+                                if cur_operator in ("*", "/"):
+                                    self.suffix_exp.append(self.operator_stack.pop())
+                                else:
+                                    break
+                            self.operator_stack.append(medial_exp[i])
+                        else:   # 中缀表达式中此时运算符是+或-
+                            while self.operator_stack:
+                                cur_operator = self.operator_stack.pop()
+                                self.operator_stack.append(cur_operator)
+                                if cur_operator in ("+", "-", "*", "/"):
+                                    self.suffix_exp.append(self.operator_stack.pop())
+                                else:
+                                    break
+                            self.operator_stack.append(medial_exp[i])
+                i += 1
+        while self.operator_stack:
+            self.suffix_exp.append(self.operator_stack.pop())
+        print(self.suffix_exp)
+
+
+s = Solution()
+s.calculate("1+2*3*4+(5-6)*(7*8+9)")
